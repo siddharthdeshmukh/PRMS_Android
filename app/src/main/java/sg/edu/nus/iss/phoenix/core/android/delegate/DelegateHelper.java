3bcfaefc56304d299c39c2b9574e896d2c736d97
Ritesh <ritesh.nus@gmail.com>
2017-09-29 01:15:08 +0800

    commit to confir my ip and port

--- app/src/main/java/sg/edu/nus/iss/phoenix/core/android/delegate/DelegateHelper.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/core/android/delegate/DelegateHelper.java
@@ -2,8 +2,8 @@ package sg.edu.nus.iss.phoenix.core.android.delegate;
 
 public class DelegateHelper {
     // RESTful parameters.
-    public final static String PRMS_BASE_URL_AUTHENTICATE = "http://192.168.0.105:8080/phoenix/rest/Login/doLogin?";
-    public final static String PRMS_BASE_URL_RADIO_PROGRAM = "http://192.168.0.105:8080/phoenix/rest/radioprogram";
-    public final static String PRMS_BASE_URL_USER = "http://192.168.0.105:8080/phoenix/rest/user";
-    public final static String PRMS_BASE_URL_SCHEDULE_PROGRAM = "http://10.0.2.2:23891/phoenix/rest/scheduleprogram";
+    public final static String PRMS_BASE_URL_AUTHENTICATE = "http://192.168.0.103:8080/phoenix/rest/Login/doLogin?";
+    public final static String PRMS_BASE_URL_RADIO_PROGRAM = "http://192.168.0.103:8080/phoenix/rest/radioprogram";
+    public final static String PRMS_BASE_URL_USER = "http://192.168.0.103:8080/phoenix/rest/user";
+    public final static String PRMS_BASE_URL_SCHEDULE_PROGRAM = "http://192.168.0.103:8080/phoenix/rest/scheduleprogram";
 }
