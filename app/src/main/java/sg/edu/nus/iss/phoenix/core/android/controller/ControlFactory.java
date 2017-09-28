8d3ec3c7d4ee4efb7182986545e582e79f098a71
thushara7 <thushara.joseph7@gmail.com>
2017-09-28 17:08:24 +0800

    [WIP]schedule Program for PRMS

--- app/src/main/java/sg/edu/nus/iss/phoenix/core/android/controller/ControlFactory.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/core/android/controller/ControlFactory.java
@@ -4,11 +4,13 @@ import sg.edu.nus.iss.phoenix.authenticate.android.controller.LoginController;
 import sg.edu.nus.iss.phoenix.radioprogram.android.controller.ProgramController;
 import sg.edu.nus.iss.phoenix.radioprogram.android.controller.ReviewSelectProgramController;
 import sg.edu.nus.iss.phoenix.user.android.controller.UserController;
+import sg.edu.nus.iss.phoenix.scheduleprogram.android.controller.ScheduleController;
 
 public class ControlFactory {
     private static MainController mainController = null;
     private static LoginController loginController = null;
     private static ProgramController programController = null;
+    private static ScheduleController scheduleController = null;
     private static ReviewSelectProgramController reviewSelectProgramController = null;
     private static UserController userController = null;
 
@@ -22,10 +24,16 @@ public class ControlFactory {
         return loginController;
     }
 
+    public static ScheduleController getScheduleController() {
+        if (scheduleController == null) scheduleController = new ScheduleController();
+        return scheduleController;
+    }
+
     public static ProgramController getProgramController() {
         if (programController == null) programController = new ProgramController();
         return programController;
     }
+
 
     public static ReviewSelectProgramController getReviewSelectProgramController() {
         if (reviewSelectProgramController == null) reviewSelectProgramController = new ReviewSelectProgramController();
